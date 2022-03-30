d3786e07eedd0633b563454797c0531729794523
Matt Pham <matthewpham135@gmail.com>
2022-03-30 17:17:50 -0500

    Finished implementing grid reset button. Project Finished.

--- script.js
+++ script.js
@@ -15,4 +15,28 @@ cell.forEach(element => {
         e => e.target.classList.add('hover')
     )
 });
+
+const reset = document.querySelector('#reset-button');
+
+reset.onclick = function (){ 
+    resetGrid();
     
+    do{
+    let x = prompt("Enter the number of squares per side: ", "16");
+    }while(x > 99 || x < 1);
+
+    let y = 0;
+    container.innerHTML = 
+    `<div class="row">${'<div class="cell"></div>'.repeat(x)}</div>`
+    .repeat(x).replace(/X/g,_=> (y++).toString(x));
+    const ncell = document.querySelectorAll('.cell');
+    ncell.forEach(element => {
+        element.addEventListener('mouseover', 
+            e => e.target.classList.add('hover')
+        )
+    });
+};
+
+function resetGrid(){
+    Array.from(cell).forEach((element) => element.classList.remove('hover'));
+}
